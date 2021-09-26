b05c65e98cc26a56c4825bf9ee5d8f266e123056
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-26 11:05:08 +0200

    final

--- Thema3_Klassen_en_Objecten_Oefeningen/Program.cs
+++ Thema3_Klassen_en_Objecten_Oefeningen/Program.cs
@@ -7,7 +7,8 @@ namespace Thema3_Klassen_en_Objecten_Oefeningen
     {
         static void Main()
         {
-            Console.WriteLine("Maak de oefeningenreeks die bij deze opdracht is opgegeven");
+            Console.WriteLine("Maak de oefeningenreeks die bij deze opdracht is opgegeven.");
+            Console.WriteLine("De opdrachten kan je downloaden via Teams of terugvinden in de git-repository.");
             Console.WriteLine("Zet de oefeningen in het bestand dat de namespace OefeningenOOP");
             Console.WriteLine("je kan deze namespace terugvinden in het bestand oefeningenOOP\n");
 
