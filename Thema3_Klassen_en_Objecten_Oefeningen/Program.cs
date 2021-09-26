3f53c98f70a115a74a41d586f40f516af0bc11f7
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-26 13:15:47 +0200

    final

--- Thema3_Klassen_en_Objecten_Oefeningen/Program.cs
+++ Thema3_Klassen_en_Objecten_Oefeningen/Program.cs
@@ -10,17 +10,17 @@ namespace Thema3_Klassen_en_Objecten_Oefeningen
             Console.WriteLine("THEMA3 - KLASSEN EN OBJECTEN - OEFENINGEN");
             Console.WriteLine("=========================================\n");
             Console.WriteLine(@"Maak de oefeningenreeks die bij deze opdracht is opgegeven (map ""solution items"").");
-            Console.WriteLine(@"De bovenstaande lijn, en ook deze lijn, is trouwens een verbatim-string. Vandaar de @ voor de start-""");
+            Console.WriteLine(@"Het bovenstaande statement, en ook dit statement, bevat trouwens een verbatim-string. Vandaar de @ voor de start-""");
             Console.WriteLine("De opdrachten kan je ook downloaden via de Teams-opdracht.");
             Console.WriteLine("\nZet de oefeningen in het bestand dat de namespace OefeningenOOP bevat");
-            Console.WriteLine("je kan deze namespace toevoegen aan Program.cs met behulp van \"using\"");
-            Console.WriteLine("De voorgaande lijn was trouwens niet verbatim. In die lijn is het escape-karakter \"\\\" gebruikt");
+            Console.WriteLine("Je kan deze namespace toevoegen aan Program.cs met behulp van \"using\"");
+            Console.WriteLine("In het voorgaande statement was de string trouwens niet verbatim. Daar is het escape-karakter \"\\\" gebruikt");
             Console.WriteLine("om rond het woord using dubbele \" te krijgen");
 
 
             Test t = new Test("mijnTestObject");
             Console.WriteLine($"\nEven testen of het aanmaken van een Test-object lukt...{t.ToString()}");
-            Console.WriteLine("De bovenstaande string was trouwens een f-string. Die herken je aan het dollar teken voor de string");
+            Console.WriteLine("De bovenstaande string was trouwens een F-string. Die herken je aan het dollarteken voor de openings \" van string");
         }
     }
 }
